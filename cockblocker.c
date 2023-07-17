56ecb6895830811d967be69008f6d44e2a841932
fendermcbender <shayke.gols@gmail.com>
2023-07-17 15:15:27 +0300

    update detection logic

--- cockblocker.c
+++ cockblocker.c
@@ -1,4 +1,5 @@
 //c0ckbl0cker.c
+//gcc -Wall -Wextra -o cock cockblocker.exe
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,14 @@ int main(void)
 {
 	FILE* command_stream;
 	char buffer[100];
+	int z, a, sites_length;
+	//format's 4 spaces, [site]\n
+	char* sites[100] = {"pornhub.com", "www.pornhub.com", "xhamster.com"};
+	for(sites_length = 0; sites[sites_length] != NULL; sites_length++)
+	{
+			printf("Sites length = %d\n", sites_length);
+	} 
+	printf("Sites length = %d\n", sites_length);
 	if((command_stream = popen("ipconfig /displaydns", "rt")) == NULL) 
 	{
 		printf("Failed to create FILE stream!\n");
@@ -19,19 +28,22 @@ int main(void)
 		{
 			fgets(buffer, 100, command_stream); //need to clean buffer of spaces if I want to compare...
 			puts(buffer);
-			if(strcmp(buffer, "    pornhub.com\n") == 0)
+			for(z = 0; z != sites_length; z++)
 			{
-				//enter detection logic
-				system("shutdown /s /t 1000");
-				break;
+				if((strstr(buffer, sites[z])) != NULL ) 
+				{
+					//enter upon detection logic
+					system("start cmd");
+					break;
+				}
 			}
-			else
-				i =+ 4
-			for(int a = 0; a < 100; a++)
+			for(a = 0; a < 100; a++)
 			{
-				buffer[a] = 0;
+				buffer[a] = '\0';
 			}
 		}
+		printf("\nFinished!\n");
+		break;
 	}
 	return 0;
 }
