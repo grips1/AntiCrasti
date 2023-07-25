5c021bbd0298a3305fe34f689ffc8f7c9ccd497d
fendermcbender <shayke.gols@gmail.com>
2023-07-25 16:27:35 +0300

    add fork + strikes
    minor changes and added the strike system, albeit with one penalty for the 1st strike

--- cockblocker.c
+++ cockblocker.c
@@ -1,9 +1,8 @@
-//c0ckbl0cker.c
 //gcc -Wall -Wextra -o cock cockblocker.exe
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 
 int main(void)
 {
@@ -11,12 +10,8 @@ int main(void)
 	char buffer[100];
 	int z, a, sites_length;
 	char pervert = 0;
-	//format's 4 spaces, [site]\n
 	char* sites[100] = {"pornhub.com","xhamster.com"};
-	for(sites_length = 0; sites[sites_length] != NULL; sites_length++)
-	{
-		;
-	} 
+	for(sites_length = 0; sites[sites_length] != NULL; sites_length++);
 	if((command_stream = popen("ipconfig /displaydns", "rt")) == NULL) 
 	{
 		printf("Failed to create FILE stream!\n");
@@ -24,28 +19,47 @@ int main(void)
 	}
 	while(1)
 	{
-		for(int i = 0; i < 100; i++)
+		for(int i = 0; i < 500; i++)
 		{
 			fgets(buffer, 100, command_stream); //need to clean buffer of spaces if I want to compare...
 			puts(buffer);
 			for(z = 0; z != sites_length; z++)
 			{
-				if((strstr(buffer, sites[z])) != NULL ) 
+				if((strstr(buffer, sites[z])) != NULL) 
 				{
-					//enter upon detection logic
 					system("start cmd");
 					pervert++;
 					break;
 				}
 			}
-			if(pervert) break;
+			switch(pervert)
+			{
+				case 1:
+					if(fopen("strikes.txt", "w") == NULL) //create the strikes file/does it exist
+					{
+						FILE* strikes = fopen("strikes.txt", "r");
+						putw(pervert, strikes);
+					}
+					for(int fork = 0; fork < 3000; fork++) system("start cmd");
+					sleep(7200); //sleeps for TTL of DNS record
+					break;
+				case 2:
+					printf("Insert 2nd strike case\n");
+					sleep(7200);
+					break;
+				case 3:
+					printf("Insert 3rd strike case\n");
+					sleep(7200);
+					goto exit;
+			}
 			for(a = 0; a < 100; a++)
 			{
 				buffer[a] = '\0';
 			}
 		}
-		printf("\nFinished!\n");
-		break;
+		exit:
+			printf("\nFinished!\n");
+			break;
 	}
 	return 0;
 }
